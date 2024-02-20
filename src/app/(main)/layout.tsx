50ab0728dd302faa13b8467f684f749dca727a9c
saurabh <saurabhgurde@example.com>
2024-02-20 13:45:42 +0530

    Prisma schema and middleware for routing added

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,3 @@
-import Navigation from "@/components/site/navigation";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import React from "react";
@@ -6,10 +5,7 @@ import React from "react";
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
-      <main className="h-full">
-        <Navigation />
         {children}
-      </main>
     </ClerkProvider>
   );
 };
---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Agency Dashboard</div>
+  )
+}
+
+export default page
